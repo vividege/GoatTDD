059263274d6b6fa3ddf91758d82b70228b7644c4
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-03 09:31:12 +0800

    assert "To-Do" in title. Add information once the assertion was failed.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -3,4 +3,6 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get("http://localhost:8000")
 
-assert 'Django' in browser.title
+assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+browser.quit()
