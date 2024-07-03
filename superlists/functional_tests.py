1e7febece72a12e2d0352010b98919bd7b415467
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-03 14:07:23 +0800

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -34,7 +34,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+                        "New to-do item did not appear in table")
 
         self.fail('Finish the test!')  # The test will fail anyway, use this way to inform that the test is complete
 
