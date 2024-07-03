35164fff0330379a513a689dec942b8a7748b32f
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-03 09:38:43 +0800

    Rewrite test using unittest way

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,8 +1,20 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
 
-browser.quit()
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finis the test!')  # The test will fail anyway, use this way to inform that the test is complete
+
+
+if __name__ == '__main__':
+    unittest.main()
