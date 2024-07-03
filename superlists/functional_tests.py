f49d408eedcae39edb92f54479c9b3827666d982
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-03 13:43:54 +0800

    Functional test now checks we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,8 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,7 +17,26 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finis the test!')  # The test will fail anyway, use this way to inform that the test is complete
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
+        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+
+        # 她按回车键后，页面更新了
+        # 待办事项表格中显示了“1: Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        self.fail('Finish the test!')  # The test will fail anyway, use this way to inform that the test is complete
 
 
 if __name__ == '__main__':
