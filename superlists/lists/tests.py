4878df6a6140588eb3688845e427e2b77f4478c5
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-04 14:55:23 +0800

    Create a dummy view method to make the test pass

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -13,10 +13,34 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        '''
+        The following code snipt was outdate due to Django version
+        :========================================================:
+
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        self.assertTemplateUsed(response, 'home.html')
+        expected_html = render_to_string('home.html', request=request)
+        print(expected_html)
+        result = response.content.decode()
+        print("============")
+        print(result)
         # self.assertTrue(response.content.startswith(b'<html>'))
         # self.assertIn(b'<title>To-Do lists</title>', response.content)
         # self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertEqual(result, expected_html)
+        '''
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+        print(expected_html)
         self.assertEqual(response.content.decode(), expected_html)
