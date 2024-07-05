98d33145bb3abe28c56944fc9e2522aa0ddb5df2
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-05 11:02:29 +0800

    Model for list Items and associated migration

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,8 @@
 from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
 # from django.core.urlresolvers import resolve # depreated since Django 1.9
 from django.urls import resolve
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -45,3 +45,22 @@ class HomePageTest(TestCase):
         # Not supported in Django 4.2
         # expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'}, request=request)
         # self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
