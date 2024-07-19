bb862822a607e60f4169f8b13dd00670d331be33
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-19 16:32:42 +0800

    before ch07: "NOT NULL contraint failed" was fixed

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -9,4 +9,4 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default='', on_delete=models.CASCADE)
+    list = models.ForeignKey(List, default='', on_delete=models.CASCADE, null=True, blank=True)
