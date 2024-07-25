0abdfd540aa8237bf413370333f2a80fd8ed6a90
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-25 16:23:13 +0800

    Hack FT runner to be able to test staging

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,14 +1,27 @@
 import time
-import unittest
 
-from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liverserver' in arg:
+                cls.server_url = 'http://'+arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
         # tell browser if needed, wait for 3 seconds
@@ -23,8 +36,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def __test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
@@ -65,7 +78,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # 弗朗西斯访问首页
         # 页面中看不到伊迪丝的清单
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -91,17 +104,17 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # 伊迪丝访问首页
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024,768)
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024, 768)
 
         # 她看到输入框完美地居中显示
-        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys("testing")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(0.2)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width']/2,
+            inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=10
         )
