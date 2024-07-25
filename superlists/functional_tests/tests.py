678c26047a50f9bc9622376d2676aaaf7936be14
hlxiao_ARAY <hlxiao@accuray.com>
2024-07-25 14:11:45 +0800

    using bootstrap and make FT pass

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,12 +2,13 @@ import time
 import unittest
 
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
         # tell browser if needed, wait for 3 seconds
