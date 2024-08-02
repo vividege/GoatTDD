c3a7347ff5b597a312de4b71c43a677d2df0e7ba
hlxiao_ARAY <hlxiao@accuray.com>
2024-08-02 20:33:21 +0800

    20240802

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -13,3 +13,10 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default='', on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
--- superlists/lists/tests/test_models.py
+++ superlists/lists/tests/test_models.py
@@ -5,34 +5,16 @@ from lists.models import Item, List
 
 
 class ListAndItemModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
+    def test_default_text(self):
+        item = Item()
+        self.assertEqual(item.text, '')
 
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = mylist
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = mylist
-        second_item.save()
-
-        saved_list = List.objects.get()
-        # saved_list: NoneType
-        # mylist: <List: List object (None)>
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, mylist)
+    def test_item_is_related_to_list(self):
+        mylist = List.objects.create()
+        item = Item()
+        item.list = mylist
+        item.save()
+        self.assertIn(item, mylist.item_set.all())
 
     def test_cannot_save_empty_list(self):
         mylist = List.objects.create()
@@ -45,3 +27,31 @@ class ListAndItemModelTest(TestCase):
     def test_get_absolute_url(self):
         mylist = List.objects.create()
         self.assertEqual(mylist.get_absolute_url(), '/lists/{0}/'.format(mylist.id))
+    def test_duplicate_items_are_invalid(self):
+        mylist = List.objects.create()
+        Item.objects.create(list=mylist, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=mylist, text='bla')
+            item.full_clean()
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean() #不该抛出异常
+
+    def test_list_ordering(self):
+
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item 2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+        )
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
